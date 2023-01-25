2e37127622d3553c516b3bb6630dd475c411356b
Neha Gupta <nehagupta150498@gmail.com>
2023-01-26 02:37:05 +0530

    css changes

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -5,31 +5,27 @@ export const Home = () => {
     const navigate = useNavigate();
     return (
         <>
-            <div className={styles["home-page"]}>
-                <div className={styles["home-details"]}>
-                    <div className={styles["home-details_heading"]}>
-                        <h1
-                            className={`margin ${styles["home-details_main-heading"]}`}
-                        >
+            <div className="section-page">
+                <div className="section-details">
+                    <div className="section-details_heading">
+                        <h1 className={`margin section-details_main-heading`}>
                             Build your resume
                         </h1>
-                        <span
-                            className={`margin ${styles["home-details_sub-heading"]}`}
-                        >
+                        <span className={`margin section-details_sub-heading`}>
                             One step near to your goal
                         </span>
                     </div>
-                    <div className={styles["home-details_call-to-action"]}>
+                    <div className="section-details_call-to-action">
                         <button
-                            className={`btn btn-dark margin  ${styles["home-details_action"]}`}
+                            className={`btn btn-dark margin  section-details_action`}
                             onClick={() => navigate("/resume")}
                         >
                             Click here
                         </button>
                     </div>
                 </div>
-                <div className={styles["home-image-parent"]}>
-                    <div className={styles["home-image"]}></div>
+                <div className="section-image-parent">
+                    <div className="section-image"></div>
                 </div>
             </div>
         </>
--- src/component/personal-info/personal-info.js
+++ src/component/personal-info/personal-info.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./personal-info.module.css";
 export const PersonalInfo = (props) => {
     const initialState = {
         name: "",
@@ -23,72 +24,82 @@ export const PersonalInfo = (props) => {
         navigate("/experience");
     };
     return (
-        <>
-            <h1>Add your personal information</h1>
-            <div className="personal-info">
-                <div className="personal-info__subfield">
-                    <label htmlFor="name">Enter your name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={user.name}
-                        onChange={(e) => handleStateChange(e)}
-                        required
-                    />
+        <div className="body-container">
+            <div className="form">
+                <h1>Add your personal information</h1>
+                <div className="personal-info">
+                    <div className="personal-info__subfield text-field">
+                        <label htmlFor="name">Enter your name:</label>
+                        <input
+                            type="text"
+                            id="name"
+                            className="input-field"
+                            name="name"
+                            value={user.name}
+                            onChange={(e) => handleStateChange(e)}
+                            required
+                        />
+                    </div>
+                    <div className="personal-info__subfield text-field">
+                        <label htmlFor="email">Enter your email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="input-field"
+                            name="email"
+                            value={user.email}
+                            onChange={(e) => handleStateChange(e)}
+                            required
+                        />
+                    </div>
+                    <div className="personal-info__subfield text-field">
+                        <label htmlFor="contact">
+                            Enter your contact no. :
+                        </label>
+                        <input
+                            type="text"
+                            id="contact"
+                            className="input-field"
+                            name="contact"
+                            value={user.contact}
+                            onChange={(e) => handleStateChange(e)}
+                            required
+                        />
+                    </div>
+                    <div className="personal-info__subfield text-field">
+                        <label htmlFor="linkedIn">
+                            Enter your linkedIn username:
+                        </label>
+                        <input
+                            type="text"
+                            id="linkedIn"
+                            className="input-field"
+                            name="linkedIn"
+                            value={user.linkedIn}
+                            onChange={(e) => handleStateChange(e)}
+                        />
+                    </div>
+                    <div className="personal-info__subfield text-field">
+                        <label htmlFor="name">Enter your github:</label>
+                        <input
+                            type="text"
+                            id="github"
+                            className="input-field"
+                            name="github"
+                            value={user.github}
+                            onChange={(e) => handleStateChange(e)}
+                        />
+                    </div>
+                    <button
+                        className="btn btn-dark"
+                        onClick={setitemToLocalStorage}
+                        disabled={!user.name || !user.email || !user.contact}
+                    >
+                        Next
+                        <span></span>
+                    </button>
                 </div>
-                <div className="personal-info__subfield">
-                    <label htmlFor="email">Enter your email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={user.email}
-                        onChange={(e) => handleStateChange(e)}
-                        required
-                    />
-                </div>
-                <div className="personal-info__subfield">
-                    <label htmlFor="contact">Enter your contact no. :</label>
-                    <input
-                        type="text"
-                        id="contact"
-                        name="contact"
-                        value={user.contact}
-                        onChange={(e) => handleStateChange(e)}
-                        required
-                    />
-                </div>
-                <div className="personal-info__subfield">
-                    <label htmlFor="linkedIn">
-                        Enter your linkedIn username:
-                    </label>
-                    <input
-                        type="text"
-                        id="linkedIn"
-                        name="linkedIn"
-                        value={user.linkedIn}
-                        onChange={(e) => handleStateChange(e)}
-                    />
-                </div>
-                <div className="personal-info__subfield">
-                    <label htmlFor="name">Enter your github:</label>
-                    <input
-                        type="text"
-                        id="github"
-                        name="github"
-                        value={user.github}
-                        onChange={(e) => handleStateChange(e)}
-                    />
-                </div>
-                <button
-                    onClick={setitemToLocalStorage}
-                    disabled={!user.name || !user.email || !user.contact}
-                >
-                    Next
-                    <span></span>
-                </button>
             </div>
-        </>
+        </div>
     );
 };
