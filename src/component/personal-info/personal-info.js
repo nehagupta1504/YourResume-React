726da99218ba7c12ff76b0c911812df37bce58a6
Neha Gupta <nehagupta150498@gmail.com>
2023-01-27 00:13:31 +0530

    css changes

--- src/component/personal-info/personal-info.js
+++ src/component/personal-info/personal-info.js
@@ -25,79 +25,139 @@ export const PersonalInfo = (props) => {
     };
     return (
         <div className="body-container">
-            <div className="form">
-                <h1>Add your personal information</h1>
-                <div className="personal-info">
-                    <div className="personal-info__subfield text-field">
-                        <label htmlFor="name">Enter your name:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            className="input-field"
-                            name="name"
-                            value={user.name}
-                            onChange={(e) => handleStateChange(e)}
-                            required
-                        />
+            <div className="form-container">
+                <div className="form">
+                    <h1>Add your personal information</h1>
+                    <div className="personal-info">
+                        <div className="personal-info__subfield text-field">
+                            <label htmlFor="name">Enter your name:</label>
+                            <input
+                                type="text"
+                                id="name"
+                                className="input-field"
+                                name="name"
+                                value={user.name}
+                                onChange={(e) => handleStateChange(e)}
+                                required
+                            />
+                        </div>
+                        <div className="personal-info__subfield text-field">
+                            <label htmlFor="email">Enter your email:</label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="input-field"
+                                name="email"
+                                value={user.email}
+                                onChange={(e) => handleStateChange(e)}
+                                required
+                            />
+                        </div>
+                        <div className="personal-info__subfield text-field">
+                            <label htmlFor="contact">
+                                Enter your contact no. :
+                            </label>
+                            <input
+                                type="text"
+                                id="contact"
+                                className="input-field"
+                                name="contact"
+                                value={user.contact}
+                                onChange={(e) => handleStateChange(e)}
+                                required
+                            />
+                        </div>
+                        <div className="personal-info__subfield text-field">
+                            <label htmlFor="linkedIn">
+                                Enter your linkedIn username:
+                            </label>
+                            <input
+                                type="text"
+                                id="linkedIn"
+                                className="input-field"
+                                name="linkedIn"
+                                value={user.linkedIn}
+                                onChange={(e) => handleStateChange(e)}
+                            />
+                        </div>
+                        <div className="personal-info__subfield text-field">
+                            <label htmlFor="name">Enter your github:</label>
+                            <input
+                                type="text"
+                                id="github"
+                                className="input-field"
+                                name="github"
+                                value={user.github}
+                                onChange={(e) => handleStateChange(e)}
+                            />
+                        </div>
+                        <button
+                            className="btn btn-dark"
+                            onClick={setitemToLocalStorage}
+                            disabled={
+                                !user.name || !user.email || !user.contact
+                            }
+                        >
+                            Next
+                            <span></span>
+                        </button>
                     </div>
-                    <div className="personal-info__subfield text-field">
-                        <label htmlFor="email">Enter your email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="input-field"
-                            name="email"
-                            value={user.email}
-                            onChange={(e) => handleStateChange(e)}
-                            required
-                        />
+                </div>
+                <div className="preview">
+                    <h1>Personal Information</h1>
+                    <div className="preview-screen">
+                        {user.name && (
+                            <div className="preview-field">
+                                <h3>
+                                    Name:{" "}
+                                    <span className="preview-field-value">
+                                        {user.name}
+                                    </span>
+                                </h3>
+                            </div>
+                        )}
+                        {user.email && (
+                            <div className="preview-field">
+                                <h3>
+                                    Email:{" "}
+                                    <span className="preview-field-value">
+                                        {user.email}
+                                    </span>
+                                </h3>
+                            </div>
+                        )}
+                        {user.contact && (
+                            <div className="preview-field">
+                                <h3>
+                                    Mob. No:{" "}
+                                    <span className="preview-field-value">
+                                        {user.contact}
+                                    </span>
+                                </h3>
+                            </div>
+                        )}
+
+                        {user.linkedIn && (
+                            <div className="preview-field">
+                                <h3>
+                                    LinkedIn:{" "}
+                                    <span className="preview-field-value">
+                                        {user.linkedIn}
+                                    </span>
+                                </h3>
+                            </div>
+                        )}
+                        {user.github && (
+                            <div className="preview-field">
+                                <h3>
+                                    Github:{" "}
+                                    <span className="preview-field-value">
+                                        {user.github}
+                                    </span>
+                                </h3>
+                            </div>
+                        )}
                     </div>
-                    <div className="personal-info__subfield text-field">
-                        <label htmlFor="contact">
-                            Enter your contact no. :
-                        </label>
-                        <input
-                            type="text"
-                            id="contact"
-                            className="input-field"
-                            name="contact"
-                            value={user.contact}
-                            onChange={(e) => handleStateChange(e)}
-                            required
-                        />
-                    </div>
-                    <div className="personal-info__subfield text-field">
-                        <label htmlFor="linkedIn">
-                            Enter your linkedIn username:
-                        </label>
-                        <input
-                            type="text"
-                            id="linkedIn"
-                            className="input-field"
-                            name="linkedIn"
-                            value={user.linkedIn}
-                            onChange={(e) => handleStateChange(e)}
-                        />
-                    </div>
-                    <div className="personal-info__subfield text-field">
-                        <label htmlFor="name">Enter your github:</label>
-                        <input
-                            type="text"
-                            id="github"
-                            className="input-field"
-                            name="github"
-                            value={user.github}
-                            onChange={(e) => handleStateChange(e)}
-                        />
-                    </div>
-                    <button
-                        className="btn btn-dark"
-                        onClick={setitemToLocalStorage}
-                        disabled={!user.name || !user.email || !user.contact}
-                    >
-                        Next
-                        <span></span>
-                    </button>
                 </div>
             </div>
         </div>
